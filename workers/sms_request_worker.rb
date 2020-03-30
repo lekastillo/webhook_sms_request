0f2c35ab6154b3911c743cd9ca95b85ee77a3dc8
Mario Gomez <mxgxw.alpha@gmail.com>
2020-03-30 19:24:03 +0300

    Added validation for response code

--- workers/sms_request_worker.rb
+++ workers/sms_request_worker.rb
@@ -14,24 +14,31 @@ class SmsRequestWorker
   def perform(sms_request_id)
     puts "::::::::::::::::::::::::::::::::::::::::::::::::::: Worker doing somthing \n"
     sms_request=SmsRequest.find(sms_request_id)
-    # TODO: Add endpoint as parameter
-    uri = URI('https://sms-scrapper.rover.quenecesito.org/sms/'+sms_request.dui)
-    # TODO: Add timeout or cancel
-    Net::HTTP.start(uri.host,uri.port,
-                    :use_ssl => uri.scheme == 'https') do |http|
-      request = Net::HTTP::Get.new uri
-      response = http.request request
-    end
-
-    # TODO: Process response or enqueue again in case of error
-    message = response.read_body
 
     if sms_request
-      resp=TwilioSms.send_sms(sms_request.phone, message)
+      # TODO: Add endpoint as parameter
+      uri = URI('https://sms-scrapper.rover.quenecesito.org/sms/'+sms_request.dui)
+    
+      # TODO: Add timeout or cancel
+      Net::HTTP.start(uri.host,uri.port,
+                    :use_ssl => uri.scheme == 'https') do |http|
+        request = Net::HTTP::Get.new uri
+        response = http.request request
+      end
 
-      sms_request.update_column(:status, 1)
-      sms_request.update_column(:last_error, resp)
-      p "The work is done: #{sms_request.inspect}"
+      if response.code == 200
+        # TODO: Process response or enqueue again in case of error
+        message = response.read_body
+
+        resp=TwilioSms.send_sms(sms_request.phone, message)
+
+        sms_request.update_column(:status, 1)
+        sms_request.update_column(:last_error, resp)
+
+        p "The work is done: #{sms_request.inspect}"
+      else
+        p "Request failed: #{sms_request.inspect}"
+      end
     end
   end
 end
